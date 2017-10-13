c3734548686ab006b2efa6ecef390a281bd8fbd0
kkadir <kadir.kilicoglu@aurea.com>
2017-10-13 21:24:44 +0300

    Add common project, clean Texpo debugger.

---
+++ Aurea.Maintenance.Debugger.Common/Utility.cs
@@ -0,0 +1,27 @@
+﻿using CIS.BusinessComponent;
+using CIS.BusinessEntity;
+using CIS.Framework.Security;
+using System.Collections.Generic;
+
+namespace Aurea.Maintenance.Debugger.Common
+{
+    public class Utility
+    {
+        public const string BillingAdminUA = "Server=SGISUSEUAV01.aesua.local,24955;Initial Catalog=saes_BillingAdmin;Trusted_Connection=Yes";
+        public const string BillingAdminDEV = "Server=SGISUSEUAV01.aesua.local,24955;Initial Catalog=daes_BillingAdmin;Trusted_Connection=Yes";
+
+        public static readonly Dictionary<string, int> Clients = new Dictionary<string, int>
+        {
+            { "TXP", 22 },
+            { "SPK", 48 }
+        };
+
+        public static GlobalApplicationConfigurationDS.GlobalApplicationConfiguration SetSecurity(string billingConection, int clientId)
+        {
+            var clientConfiguration = GlobalApplicationConfigurationBC.Load(billingConection, clientId);
+            SecurityManager.SetSecurityContext(clientConfiguration, clientId);
+            
+            return clientConfiguration;
+        }
+    }
+}
--- Aurea.Maintenance.Debugger.Texpo/Program.cs
+++ Aurea.Maintenance.Debugger.Texpo/Program.cs
@@ -1,38 +1,13 @@
-﻿using CIS.BusinessComponent;
-using CIS.BusinessEntity;
-using CIS.Framework.Security;
-using CIS.Import.Billing;
-using System.Collections.Generic;
+﻿using Aurea.Maintenance.Debugger.Common;
 
 namespace Aurea.Maintenance.Debugger.Texpo
 {
     class Program
     {
-        private const string BillingAdminUA = "Server=SGISUSEUAV01.aesua.local,24955;Initial Catalog=saes_BillingAdmin;Trusted_Connection=Yes";
-        private const string BillingAdminDEV = "Server=SGISUSEUAV01.aesua.local,24955;Initial Catalog=daes_BillingAdmin;Trusted_Connection=Yes";
-        private static readonly Dictionary<string, int> Clients = new Dictionary<string, int>
-        {
-            { "TXP", 22 },
-            { "SPK", 48 }
-        };
 
         static void Main(string[] args)
         {
-            var clientConfiguration = SetSecurity(BillingAdminDEV, Clients["TXP"]);
-
-            Tdsp tdspQueue = new Tdsp(clientConfiguration.ConnectionMarket, clientConfiguration.ConnectionCsr, BillingAdminDEV, clientConfiguration.ClientAbbreviation);
-            tdspQueue.Import();
-            
-            
-
-        }
-
-        private static GlobalApplicationConfigurationDS.GlobalApplicationConfiguration SetSecurity(string billingConection, int clientId)
-        {
-            var clientConfiguration = GlobalApplicationConfigurationBC.Load(billingConection, clientId);
-            SecurityManager.SetSecurityContext(clientConfiguration, clientId);
-
-            return clientConfiguration;
+            var clientConfiguration = Utility.SetSecurity(Utility.BillingAdminDEV, Utility.Clients["TXP"]);
         }
     }
 }
