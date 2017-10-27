12d9c45ae030ea08efae25b55f96fa6a48a8183a
Sabri Arslan <sabriarslanversata@users.noreply.github.com>
2017-10-27 11:17:00 +0300

    added 814E inbound simulation

--- Aurea.Maintenance.Debugger.Stream/Program.cs
+++ Aurea.Maintenance.Debugger.Stream/Program.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 using Aurea.Maintenance.Debugger.Common;
+
+using CIS.BusinessEntity;
+//using CIS.Element.Billing;
+//using Csla.Validation;
 
 namespace Aurea.Maintenance.Debugger.Stream
 {
@@ -12,7 +18,37 @@ namespace Aurea.Maintenance.Debugger.Stream
         static void Main(string[] args)
         {
             var clientConfiguration = Utility.SetSecurity(Utility.BillingAdminDEV, Utility.Clients["SGE"]);
-            
+
+            //SimulatePostEnrollmentEvent(clientConfiguration);
+
+            //SimulateInbound814E(clientConfiguration);
+        }
+
+        private static void SimulateInbound814E(GlobalApplicationConfigurationDS.GlobalApplicationConfiguration clientConfiguration)
+        {
+            // Set culture to en-EN to prevent string manipulation issues in base code
+            SetThreadCulture("en-EN");
+
+            var engine = new CIS.Engine.Event.Queue(Utility.BillingAdminDEV);
+            engine.ProcessEventQueue(clientConfiguration.ClientID, clientConfiguration.ConnectionCsr, clientConfiguration.ConnectionMarket, clientConfiguration.ClientAbbreviation);
+        }
+
+        private static void SimulatePostEnrollmentEvent(GlobalApplicationConfigurationDS.GlobalApplicationConfiguration clientConfiguration)
+        {
+            // Set culture to en-EN to prevent string manipulation issues in base code
+            SetThreadCulture("en-EN");
+
+            var pe = new CIS.Clients.Stream.PostEnrollment
+            {
+                ConnectionString = clientConfiguration.ConnectionCsr
+            };
+            pe.Process(30396997);
+        }
+
+        private static SetThreadCulture(string culture)
+        {
+            Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.GetCultureInfo(culture);
+            Thread.CurrentThread.CurrentUICulture = System.Globalization.CultureInfo.GetCultureInfo(culture);
         }
     }
 }
