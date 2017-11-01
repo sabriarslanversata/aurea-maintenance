a7e6352bf5911a59b1338dfaae7eb19f98087fb3
Sabri Arslan <sabriarslanversata@users.noreply.github.com>
2017-11-01 10:11:03 +0300

    added even generator call and event handler call into texpo debugger

--- Aurea.Maintenance.Debugger.Texpo/Program.cs
+++ Aurea.Maintenance.Debugger.Texpo/Program.cs
@@ -1,30 +1,32 @@
 ﻿using System;
+using System.Collections;
 using System.Runtime.Remoting.Contexts;
+using System.Threading;
 using Aurea.Logging;
 using Aurea.Maintenance.Debugger.Common;
 using Aurea.TaskToaster;
 using CIS.BusinessEntity;
-using Aurea.TaskToaster.ProductTasks;
 
 namespace Aurea.Maintenance.Debugger.Texpo
 {
     public class Program
     {
-        private static TaskContext CreateContext(
-            GlobalApplicationConfigurationDS.GlobalApplicationConfiguration configuration)
+        private static GlobalApplicationConfigurationDS.GlobalApplicationConfiguration _clientConfiguration;
+
+        private static TaskContext CreateContext()
         {
             return new TaskContext
             {
-                EnvironmentId = configuration.EnvironmentID, // Dev = 1, Qc = 2, Ua = 12, Production = 13
+                EnvironmentId = _clientConfiguration.EnvironmentID, // Dev = 1, Qc = 2, Ua = 12, Production = 13
                 BillingAdminConnection = Utility.BillingAdminDEV,
                 Client = "Texpo",
                 ClientName = "Texpo",
                 Namespace = "Texpo",
                 Abbreviation = "TXP",
                 ClientId = 22,
-                ClientConnection = configuration.ConnectionCsr,
-                MarketConnection = configuration.ConnectionMarket,
-                TDSPConnection = configuration.ConnectionTdsp,
+                ClientConnection = _clientConfiguration.ConnectionCsr,
+                MarketConnection = _clientConfiguration.ConnectionMarket,
+                TDSPConnection = _clientConfiguration.ConnectionTdsp,
                 TaxConnection = "",
                 Logger = new ConsoleLogger()
             };
@@ -32,17 +34,50 @@ namespace Aurea.Maintenance.Debugger.Texpo
 
         public static void Main(string[] args)
         {
-            var clientConfiguration = Utility.SetSecurity(Utility.BillingAdminDEV, Utility.Clients["TXP"]);
+            _clientConfiguration = Utility.SetSecurity(Utility.BillingAdminDEV, Utility.Clients["TXP"]);
+            
+            // Set culture to en-EN to prevent string manipulation issues in base code
+            SetThreadCulture("en-US");
+
+            GenerateSimpleMarketTransactionEvaluationEvents();
+            ProcessEvents();
             /*
 			CalculateConsumptionDueDatesTask myTask = new CalculateConsumptionDueDatesTask();
 
-            myTask.Initialize(CreateContext(clientConfiguration));
+            myTask.Initialize(CreateContext());
             
             //execute Maintenance.CalculateConsumptionDueDates 
             myTask.Execute();
 			*/
             var waitForLine = Console.ReadLine();
             Console.WriteLine($"readed {waitForLine}");
+        }
+
+        private static void GenerateSimpleMarketTransactionEvaluationEvents()
+        {
+            var gen = new CIS.Framework.Event.EventGenerator.SimpleMarketTransactionEvaluation(_clientConfiguration.ConnectionCsr, Utility.BillingAdminDEV);
+            var hashTable = new Hashtable();
+
+            if (gen.Generate(Utility.Clients["TXP"], hashTable))
+            {
+                Console.WriteLine("Events Generated");
+            }
+            else
+            {
+                Console.WriteLine("Events could not generated");
+            }
+        }
+
+        private static void ProcessEvents()
+        {
+            var engine = new CIS.Engine.Event.Queue(Utility.BillingAdminDEV);
+            engine.ProcessEventQueue(_clientConfiguration.ClientID, _clientConfiguration.ConnectionCsr, _clientConfiguration.ConnectionMarket, _clientConfiguration.ClientAbbreviation);
+        }
+
+        private static void SetThreadCulture(string culture)
+        {
+            Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.GetCultureInfo(culture);
+            Thread.CurrentThread.CurrentUICulture = System.Globalization.CultureInfo.GetCultureInfo(culture);
         }
     }
 }
