97fe3416f331a28898d0bf6c7adc7d9b8eec373d
Sabri Arslan <sabriarslanversata@users.noreply.github.com>
2017-11-23 10:54:16 +0300

    init for 17147

---
+++ Aurea.Maintenance.Debugger.Common/Utility.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aurea.Maintenance.Debugger.Common
+{
+    public class Utility
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        // ReSharper disable once InconsistentNaming
+        private struct SYSTEMTIME
+        {
+            public short wYear;
+            public short wMonth;
+            // ReSharper disable once MemberCanBePrivate.Local
+            // ReSharper disable once FieldCanBeMadeReadOnly.Local
+            public short wDayOfWeek;
+            public short wDay;
+            public short wHour;
+            public short wMinute;
+            public short wSecond;
+            // ReSharper disable once FieldCanBeMadeReadOnly.Local
+            // ReSharper disable once MemberCanBePrivate.Local
+            public short wMilliseconds;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool SetSystemTime(ref SYSTEMTIME st);
+
+        public static void ChangeSystemDateTime(DateTime value)
+        {
+            // ReSharper disable once UseObjectOrCollectionInitializer
+            var st = new SYSTEMTIME();
+            st.wYear = (short)value.Year;
+            st.wMonth = (short)value.Month;
+            st.wDay = (short)value.Day;
+            st.wHour = (short)value.Hour;
+            st.wMinute = (short)value.Minute;
+            st.wSecond = (short)value.Second;
+            SetSystemTime(ref st);
+        }
+    }
+}
