0818bcb2742d15e48dd421d052ece42217d11e45
Sabri Arslan <sabriarslanversata@users.noreply.github.com>
2017-12-06 11:45:55 +0300

    Added TransactionMode settings to prevent DTC promoting on second Sql Connection open

--- Aurea.Maintenance.Debugger.Common/Models/ClientEnvironmentConfiguration.cs
+++ Aurea.Maintenance.Debugger.Common/Models/ClientEnvironmentConfiguration.cs
@@ -4,21 +4,24 @@
 
     public class ClientEnvironmentConfiguration
     {
-        private const string _billingAdminConnectionString = "Server={0}.{1},24955;Initial Catalog={2}_BillingAdmin;Trusted_Connection=Yes";
+        private const string _billingAdminConnectionString = "Server={0}.{1},24955;Initial Catalog={2}_BillingAdmin;Trusted_Connection=Yes{3}";
 
-        public ClientEnvironmentConfiguration(Clients client, Stages stage)
+        public ClientEnvironmentConfiguration(Clients client, Stages stage, TransactionMode transactionMode)
         {
             Client = client.Abbreviation();
             ClientId = client.Id();
             Stage = stage.Name();
             Server = stage.Server();
-            ConnectionBillingAdmin = string.Format(_billingAdminConnectionString, stage.Server(), stage.Domain(), stage.Prefix());
+            TransactionMode = transactionMode;
+            ConnectionBillingAdmin = string.Format(_billingAdminConnectionString, stage.Server(), stage.Domain(), stage.Prefix(),
+                transactionMode == TransactionMode.Enlist ? ";Enlist=false" : "");
         }
 
         public string Client { get; set; }
         public int ClientId { get; set; }
         public string Stage { get; set; }        
         public string Server { get; set; }
+        public TransactionMode TransactionMode { get; set; }
         public string ConnectionBillingAdmin { get; set; }
     }
 }
